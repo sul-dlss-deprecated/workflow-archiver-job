85b49c54360b0c5cec5d6cc3db918f75ecbe40bf
Willy Mene <wmene@stanford.edu>
2013-06-17 10:24:17 -0700

    Use default ruby instead of specific patchleve in crontab entry

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 set :output, '/home/lyberadmin/workflow-archiver-job/current/log/crondebug.log'
 
 every :day, :at => '2:16am', :roles => [:app]  do
- command "BUNDLE_GEMFILE=/home/lyberadmin/workflow-archiver-job/current/Gemfile ROBOT_ENVIRONMENT=#{environment} /usr/local/rvm/wrappers/ruby-1.8.7-p370/ruby /home/lyberadmin/workflow-archiver-job/current/bin/run_archiver"
+ command "BUNDLE_GEMFILE=/home/lyberadmin/workflow-archiver-job/current/Gemfile ROBOT_ENVIRONMENT=#{environment} /usr/local/rvm/wrappers/default/ruby /home/lyberadmin/workflow-archiver-job/current/bin/run_archiver"
 end
