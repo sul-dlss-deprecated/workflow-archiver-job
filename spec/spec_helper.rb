0bf108153e48c7d54612b85952cc1477a587c1d8
Willy Mene <wmene@stanford.edu>
2014-02-07 15:27:29 -0800

    Add params for integration tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 # Make sure specs run with the definitions from test.rb
-environment = ENV['ROBOT_ENVIRONMENT'] = 'test'
+environment = ENV.fetch('ROBOT_ENVIRONMENT', 'test')
 
 bootfile = File.expand_path(File.dirname(__FILE__) + '/../config/boot')
 require bootfile
