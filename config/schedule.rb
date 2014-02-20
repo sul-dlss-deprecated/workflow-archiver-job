3448788ee3e9dc52cf680a3c59923d7ea6d6db51
Willy Mene <wmene@stanford.edu>
2014-02-20 11:08:33 -0800

    Run archiver every 15 minutes via cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 set :output, '/home/lyberadmin/workflow-archiver-job/current/log/crondebug.log'
 
-every :day, :at => '2:16am', :roles => [:app]  do
+every 15.minutes, :roles => [:app]  do
  command "BUNDLE_GEMFILE=/home/lyberadmin/workflow-archiver-job/current/Gemfile ROBOT_ENVIRONMENT=#{environment} /usr/local/rvm/wrappers/default/ruby /home/lyberadmin/workflow-archiver-job/current/bin/run_archiver"
 end
