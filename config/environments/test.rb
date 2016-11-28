ff76c4764873c93a1916ba8114a45dfe44f111fd
Darren Hardy <drh@stanford.edu>
2016-11-28 12:43:47 -0800

    need oracle method and user/pw in db_uri

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,6 +1,4 @@
 Dor::WorkflowArchiver.config.configure do
-  db_login          'user'
-  db_password       'passwd'
-  db_uri            '//example.com:1521/example'
+  db_uri            'oracle://user:password@example.com:1521/example'
   dor_service_uri   'https://example.com'
 end
