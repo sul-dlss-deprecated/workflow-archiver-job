46b1d21bb7f9a57755aaabdc1e891243436551bf
Justin Coyne <jcoyne@justincoyne.com>
2019-03-04 07:52:16 -0600

    Disable the cron in prod

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -3,4 +3,9 @@ server 'sul-lyberservices-prod.stanford.edu', user: 'lyberadmin', roles: %w{web 
 Capistrano::OneTimeKey.generate_one_time_key!
 
 set :whenever_environment, 'production'
+
+# This prevents prod from updating the crontab.
+# Adding this because we are deploying the rails-workflow-server which does not need archiving
+Rake::Task["whenever:update_crontab"].clear_actions
+
 set :bundle_without, 'deployment'
